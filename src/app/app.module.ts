1e8e99a083ab0f44c5f346d32970b4689c8db00e
Andres Alban <bethesdas101@gmail.com>
2020-09-23 18:48:02 -0500

    Adding initial structure for carrito app with its styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductosComponent } from './productos/productos.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { OrdenesComponent } from './ordenes/ordenes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent,
+    CarritoComponent,
+    NavbarComponent,
+    OrdenesComponent
   ],
   imports: [
     BrowserModule
