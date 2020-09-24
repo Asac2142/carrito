686fb32f62d8bebda09c501dfd8fb5a105e9a644
Andres Alban <bethesdas101@gmail.com>
2020-09-23 23:30:58 -0500

    Adding notification module to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ProductosComponent } from './productos/productos.component';
 import { CarritoComponent } from './carrito/carrito.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { OrdenesComponent } from './ordenes/ordenes.component';
+import { SimpleNotificationsModule } from 'angular2-notifications';
 
 @NgModule({
   declarations: [
@@ -17,8 +20,12 @@ import { OrdenesComponent } from './ordenes/ordenes.component';
     OrdenesComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule, 
+    FormsModule,
+    SimpleNotificationsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
